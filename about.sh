bbdcfd60edd99e23a94810b9928e200dc9da90b1
Dacung555 <36485855+Dacung555@users.noreply.github.com>
2018-03-10 00:17:03 +0700

    Create about.sh

---
+++ about.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#About this script
+
+clear
+echo -e "Script Auto Install SSH & OpenVPN v2.0" 
+echo -e ""
+echo -e "For Debian 7 32 bit & 64 bit"
+echo -e "For VPS with KVM and VMWare virtualization"
+echo -e ""
+echo -e "Original script by :"
+echo -e "* Fornesia"
+echo -e "* Rzengineer"
+echo -e "* Fawzya"
+echo -e ""
+echo -e "Mod by white-vps.com"
+echo -e ""
+echo -e "https://www.white-vps.com"
+echo -e "https://www.white-vps.com"
+echo -e ""
