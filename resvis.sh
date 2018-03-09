6c83523fba7399746b0487cd7115211ba3b2e5c3
Dacung555 <36485855+Dacung555@users.noreply.github.com>
2018-03-10 00:10:33 +0700

    Create resvis.sh

---
+++ resvis.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Script restart service dropbear, webmin, squid3, openvpn, openssh
+# Created by Bustami Arifin
+service dropbear restart
+service stunnel4 restart
+service webmin restart
+service squid3 restart
+service openvpn restart
+service ssh restart
+service openvpn status
