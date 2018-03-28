3244448f988cd5afe0b40d3a79b894c3f050e80d
Dacung555 <36485855+Dacung555@users.noreply.github.com>
2018-03-29 00:40:56 +0700

    Update ocs-panel-debian7.sh

--- ocs-panel-debian7.sh
+++ ocs-panel-debian7.sh
@@ -30,8 +30,8 @@ apt-get install nginx php5 php5-fpm php5-cli php5-mysql php5-mcrypt
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
 mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.old
-curl http://kangismet.net/script/ocs/ngix.conf > /etc/nginx/nginx.conf
-curl http://kangismet.net/script/ocs/vps.conf > /etc/nginx/conf.d/vps.conf
+curl https://raw.githubusercontent.com/Dacung555/setup-ssh-dan-vpn/master/nginx.conf > /etc/nginx/nginx.conf
+curl https://raw.githubusercontent.com/Dacung555/setup-ssh-dan-vpn/master/vps.conf > /etc/nginx/conf.d/vps.conf
 sed -i 's/cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php5/fpm/php.ini
 sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
 
