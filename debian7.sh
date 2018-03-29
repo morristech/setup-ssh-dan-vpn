349a36009ed92765fcc3aafb87ecbbe01085615d
Dacung555 <36485855+Dacung555@users.noreply.github.com>
2018-03-30 02:49:32 +0700

    Update debian7.sh

--- debian7.sh
+++ debian7.sh
@@ -79,7 +79,7 @@ wget -O /etc/openvpn/1194.conf "https://raw.githubusercontent.com/Dacung555/setu
 service openvpn restart
 sysctl -w net.ipv4.ip_forward=1
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
-iptables -t nat -I POSTROUTING -s 192.168.100.0/24 -o eth0 -j MASQUERADE
+iptables -t nat -I POSTROUTING -s 10.0.0.0/8 -o eth0 -j MASQUERADE
 iptables-save > /etc/iptables_yg_baru_dibikin.conf
 wget -O /etc/network/if-up.d/iptables "https://raw.githubusercontent.com/Dacung555/setup-ssh-dan-vpn/master/iptables-restore"
 chmod +x /etc/network/if-up.d/iptables
