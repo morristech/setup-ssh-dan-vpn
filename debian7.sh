ffd7f9d324baacaf7ee74af35317f180943c0a53
Dacung555 <36485855+Dacung555@users.noreply.github.com>
2018-03-30 02:47:24 +0700

    Update debian7.sh

--- debian7.sh
+++ debian7.sh
@@ -81,7 +81,7 @@ sysctl -w net.ipv4.ip_forward=1
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
 iptables -t nat -I POSTROUTING -s 192.168.100.0/24 -o eth0 -j MASQUERADE
 iptables-save > /etc/iptables_yg_baru_dibikin.conf
-wget -O /etc/network/if-up.d/iptables "http://vira.cf/iptables"
+wget -O /etc/network/if-up.d/iptables "https://raw.githubusercontent.com/Dacung555/setup-ssh-dan-vpn/master/iptables-restore"
 chmod +x /etc/network/if-up.d/iptables
 service openvpn restart
 
