087a7994ef7cb9e9909fbf3353811f71d865ed49
suzukismash <36485855+Dacung555@users.noreply.github.com>
2018-05-26 04:54:03 +0700

    Update php5.sh

--- php5.sh
+++ php5.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 #
 
-php5-cgi
-php5-cli 
-php5-common 
-php5-curl 
-php5-dbg 
-php5-dev 
-php5-enchant
-php5-fpm 
-php5-gd 
-php5-gmp 
-php5-imap 
-php5-interbase
-php5-intl 
-php5-ldap 
-php5-mcrypt 
-php5-mysql 
-php5-mysqlnd 
-php5-odbc 
-php5-pgsql 
-php5-pspell 
-php5-recode
-php5-snmp 
-php5-sqlite 
-php5-sybase 
-php5-tidy 
-php5-xmlrpc 
-php5-xsl 
-php5-librdf 
-php5-remctl 
-php5-xcache 
-php5-xdebug 
+sudo apt-get install php5-cgi
+sudo apt-get install php5-cli 
+sudo apt-get install php5-common 
+sudo apt-get install php5-curl 
+sudo apt-get install php5-dbg 
+sudo apt-get install php5-dev 
+sudo apt-get install php5-enchant
+sudo apt-get install php5-fpm 
+sudo apt-get install php5-gd 
+sudo apt-get install php5-gmp 
+sudo apt-get install php5-imap 
+sudo apt-get install php5-interbase
+sudo apt-get install php5-intl 
+sudo apt-get install php5-ldap 
+sudo apt-get install php5-mcrypt 
+sudo apt-get install php5-mysql 
+sudo apt-get install php5-mysqlnd 
+sudo apt-get install php5-odbc 
+sudo apt-get install php5-pgsql 
+sudo apt-get install php5-pspell 
+sudo apt-get install php5-recode
+sudo apt-get install php5-snmp 
+sudo apt-get install php5-sqlite 
+sudo apt-get install php5-sybase 
+sudo apt-get install php5-tidy 
+sudo apt-get install php5-xmlrpc 
+sudo apt-get install php5-xsl 
+sudo apt-get install php5-librdf 
+sudo apt-get install php5-remctl 
+sudo apt-get install php5-xcache 
+sudo apt-get install php5-xdebug 
