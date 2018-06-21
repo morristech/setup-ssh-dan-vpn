a24d65fa15c220daa449a49be46ee0eeb7efd6fa
suzukismash <36485855+Dacung555@users.noreply.github.com>
2018-06-22 06:11:08 +0700

    Update usernew.sh

--- usernew.sh
+++ usernew.sh
@@ -5,7 +5,7 @@ read -p "Username : " Login
 read -p "Password : " Pass
 read -p "Expired (hari): " masaaktif
 
-IP=`dig +short myip.opendns.com @resolver1.opendns.com`
+IP=`curl icanhazip.com`
 useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
 exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
