0d8fbc812d4f75842bff385c74316142321a7f52
suzukismash <36485855+Dacung555@users.noreply.github.com>
2018-07-02 07:03:24 +0700

    Update debian7.sh

--- debian7.sh
+++ debian7.sh
@@ -131,30 +131,30 @@ apt-get -y install webmin
 sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
 
 # install stunnel
-apt-get install stunnel4 -y
-cat > /etc/stunnel/stunnel.conf <<-END
-cert = /etc/stunnel/stunnel.pem
-client = no
-socket = a:SO_REUSEADDR=1
-socket = l:TCP_NODELAY=1
-socket = r:TCP_NODELAY=1
+# apt-get install stunnel4 -y
+# cat > /etc/stunnel/stunnel.conf <<-END
+# cert = /etc/stunnel/stunnel.pem
+# client = no
+# socket = a:SO_REUSEADDR=1
+# socket = l:TCP_NODELAY=1
+# socket = r:TCP_NODELAY=1
 
 
-[dropbear]
-accept = 443
-connect = 127.0.0.1:1080
+# [dropbear]
+# accept = 443
+# connect = 127.0.0.1:1080
 
-END
+# END
 
-#membuat sertifikat
-openssl genrsa -out key.pem 2048
-openssl req -new -x509 -key key.pem -out cert.pem -days 1095 \
--subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
-cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
+# membuat sertifikat
+# openssl genrsa -out key.pem 2048
+# openssl req -new -x509 -key key.pem -out cert.pem -days 1095 \
+# -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
+# cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
 
 #konfigurasi stunnel
-sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
-/etc/init.d/stunnel4 restart
+# sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
+# /etc/init.d/stunnel4 restart
 
 # install fail2ban
 apt-get -y install fail2ban;service fail2ban restart
@@ -218,7 +218,7 @@ cd
 chown -R www-data:www-data /home/vps/public_html
 /etc/init.d/ssh restart
 /etc/init.d/dropbear restart
-/etc/init.d/stunnel4 restart
+# /etc/init.d/stunnel4 restart
 /etc/init.d/squid3 restart
 /etc/init.d/nginx restart
 /etc/init.d/openvpn restart
